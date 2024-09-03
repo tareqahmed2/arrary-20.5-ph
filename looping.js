cc3bd1811d751d90dc7165378e6b99914c5924b8
tareq ahmed <tareqahmed347396@gmail.com>
2024-09-03 20:30:10 +0600

    mmodule 20.5 array

---
+++ looping.js
@@ -0,0 +1,13 @@
+// looping technique
+// 1.for loop
+// 2.while loop
+// 3.do while loop
+// 4.for of array loop korar jonno
+// 5.for in object loop korar jonno
+const friends = ["elon", "bill", "mark", "maren"];
+for (const friend of friends) {
+  console.log(friend);
+}
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
